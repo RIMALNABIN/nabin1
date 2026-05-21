6487c7c9c5aca9fd947cea5fd0a87594209d6df7
RIMALNABIN <y252094@yamaguchi-jc.ac.jp>
2026-05-21 05:31:27 +0000

    yes

---
+++ work4/2.py
@@ -0,0 +1,6 @@
+scores = [80, 55, 90, 40]
+
+def average(numbers):
+    return sum(numbers) / len(numbers)
+
+print(average(scores))
---
+++ work4/4.py
@@ -0,0 +1,4 @@
+def greeting(name, country):
+    print(f"Hello {name} from {country}")
+
+greeting("Taro", "Japan")
